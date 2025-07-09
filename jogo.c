d92fb144ddb3456068fe6c4b70737359c09e387b
Giulia Tafuri <113373852+gtafuri@users.noreply.github.com>
2025-07-09 20:36:56 -0300

    Create jogo.c
    modularizando

---
+++ jogo.c
@@ -0,0 +1,13 @@
+#include "jogo.h"
+
+void inicializarJogo(Jogador *jogador, Mapa *mapa) {
+    // Inicialização do estado do jogo
+}
+
+void atualizarJogo(Jogador *jogador, Mapa *mapa) {
+    // Atualização da lógica do jogo
+}
+
+void liberarJogo(Jogador *jogador, Mapa *mapa) {
+    // Liberação de recursos
+} 
